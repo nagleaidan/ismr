c8d898f627eed848a93c2e576f8e9e7c5c45b4a3
Aidan Nagle <nagleaidan@gmail.com>
2020-07-13 06:11:34 -0400

    I'm having way too much fun finding pictures of Stephen

--- js/main.js
+++ js/main.js
@@ -34,14 +34,15 @@ function fadeInText() {
 }
 
 function pickRandomPic() {
-	// this gives a 4x4% chance for the picture of Stephen to not be a professionally looking picture
+	// this gives a 5x3% chance for the picture of Stephen to not be a professionally looking picture
 	// and instead be a goofy looking picture from some of his videos on the 512Pixels YouTube channel
 	const pic = document.getElementById('hackett');
-	const rand = Math.floor(Math.random() * 25);
+	const rand = Math.floor(Math.random() * 100 / 3);
 	if (rand === 0) pic.setAttribute('src', './images/hackett_boom.png');
 	if (rand === 1) pic.setAttribute('src', './images/hackett_hey.png');
 	if (rand === 2) pic.setAttribute('src', './images/hackett_mask.png');
 	if (rand === 3) pic.setAttribute('src', './images/hackett_shrug.png');
+	if (rand === 4) pic.setAttribute('src', './images/hackett_twoPhones.png');
 	// I set the img to have `display: none` and remove that once the picture has been changed
 	// this is to prevent flashes of the main picture if you get one of the goofy ones
 	pic.style.removeProperty('display');
