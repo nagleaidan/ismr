9a9cf5206fdb03926fbd9f8c183c70a58d499faa
Aidan Nagle <nagleaidan@gmail.com>
2020-07-12 23:08:21 -0400

    header

--- js/main.js
+++ js/main.js
@@ -9,61 +9,61 @@ let prophecyTitle = document.getElementById("prophecy-tit");
 let oracleImage = document.getElementById("oracle-img");
 
 function playRandomProphecy() {
-    lastProphecy = getRandomElement(prophecies);
-    playAudio(lastProphecy.audio);
-    updateText();
+	lastProphecy = getRandomElement(prophecies);
+	playAudio(lastProphecy.audio);
+	updateText();
 }
 
 function updateText() {
-    window.clearTimeout(textFadeTimeout);
-    window.clearTimeout(interpretationFadeTimeout);
+	window.clearTimeout(textFadeTimeout);
+	window.clearTimeout(interpretationFadeTimeout);
 
-    oracleImage.style.opacity = 1;
+	oracleImage.style.opacity = 1;
 
-    text.style.opacity = 0;
-    interpretation.style.opacity = 0;
-    prophecyTitle.style.opacity = 0;
-    textFadeTimeout = window.setTimeout(fadeInText, 600);
-    interpretationFadeTimeout = window.setTimeout(fadeInInterpretation, 1200);
+	text.style.opacity = 0;
+	interpretation.style.opacity = 0;
+	prophecyTitle.style.opacity = 0;
+	textFadeTimeout = window.setTimeout(fadeInText, 600);
+	interpretationFadeTimeout = window.setTimeout(fadeInInterpretation, 1200);
 }
 
 function fadeInText() {
-    text.innerText = lastProphecy.text;
-    text.style.opacity = 1;
+	text.innerText = lastProphecy.text;
+	text.style.opacity = 1;
 }
 
 function fadeInInterpretation() {
-    prophecyTitle.style.opacity = targetOpacity;
-    interpretation.innerText = interpretations[lastProphecy.interpretation];
-    if (lastProphecy.interpretation) {
-        interpretation.style.opacity = targetOpacity;
-    }
+	prophecyTitle.style.opacity = targetOpacity;
+	interpretation.innerText = interpretations[lastProphecy.interpretation];
+	if (lastProphecy.interpretation) {
+		interpretation.style.opacity = targetOpacity;
+	}
 }
 
 function playDivineOracleSound() {
-    if (oracleImage.style.opacity == 0) {
-        return;
-    }
-    let more = ["06", "15", "40"];
-    let randomSound = getRandomElement(more);
-    playAudio(randomSound);
-    oracleImage.style.transition = "transform 0.175s linear";
-    oracleImage.style.transform = "scale(1.1)";
-    window.setTimeout(() => {
-        oracleImage.style.transform = "scale(1.0)";
-    }, 175);
+	if (oracleImage.style.opacity == 0) {
+		return;
+	}
+	let more = ["06", "15", "40"];
+	let randomSound = getRandomElement(more);
+	playAudio(randomSound);
+	oracleImage.style.transition = "transform 0.175s linear";
+	oracleImage.style.transform = "scale(1.1)";
+	window.setTimeout(() => {
+		oracleImage.style.transform = "scale(1.0)";
+	}, 175);
 }
 
 function playAudio(name) {
-    let data = sounds[name];
-    let audio = new Audio(data);
-    audio.play();
+	let data = sounds[name];
+	let audio = new Audio(data);
+	audio.play();
 }
 
 function replayLastProphecy() {
-    playAudio(lastProphecy.audio);
+	playAudio(lastProphecy.audio);
 }
 
 function getRandomElement(x) {
-    return x[Math.floor(Math.random() * x.length)];
+	return x[Math.floor(Math.random() * x.length)];
 }
