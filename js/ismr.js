de550916dc4a80cd66a67edda8240cc8e6f64818
Aidan Nagle <nagleaidan@gmail.com>
2020-07-14 08:00:37 -0400

    add all of 278 and 280

--- js/ismr.js
+++ js/ismr.js
@@ -1,8 +1,38 @@
 let sounds = [
-    {
-        'episode': 270,
-        'timestamp': '7:53',
-        "text": 'asmr',
-        "filepath": '278_1.mp3'
-    }
+	{
+		'episode': 278,
+		'timestamp': '7:53',
+		"text": 'asmr',
+		"filepath": '278_1.mp3'
+	},
+	{
+		'episode': 278,
+		'timestamp': '15:46',
+		"text": 'the shownotes',
+		"filepath": '278_2.mp3'
+	},
+	{
+		'episode': 278,
+		'timestamp': '29:40',
+		"text": 'the fbi',
+		"filepath": '278_3.mp3'
+	},
+	{
+		'episode': 278,
+		'timestamp': '1:10:30',
+		"text": 'pokemon',
+		"filepath": '278_4.mp3'
+	},
+	{
+		'episode': 278,
+		'timestamp': '1:27:39',
+		"text": 'pwas',
+		"filepath": '278_5.mp3'
+	},
+	{
+		'episode': 280,
+		'timestamp': '1:17:39',
+		"text": 'gamify your life',
+		"filepath": '280_1.mp3'
+	},
 ]
